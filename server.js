05242e9de03d9f11eea8a767e3a31e9da18d7cbf
rkhosroshahli <rasa.khosrowshahli@ontariotechu.net>
2023-02-28 12:10:17 -0500

    deleted /

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use('/api/users', usersRoutes);
 
 __dirname=path.resolve();
 if (process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join('/frontend')));
+  app.use(express.static(path.join('frontend')));
   app.get('*', (req, res)=>{
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   });
