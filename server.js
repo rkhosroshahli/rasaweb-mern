404b16ac598d7bf36985a7331802ed81068c8a6f
rkhosroshahli <rasa.khosrowshahli@ontariotechu.net>
2023-02-28 12:38:55 -0500

    is this the problem?

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
